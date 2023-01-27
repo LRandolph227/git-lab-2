f0fcab9744aa02d67a368eade7aec789637b413a
Lucas Randolph <lr939421@ohio.edu>
2023-01-27 15:02:18 -0500

    Program commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Lucas Randolph
+ *        Date: 1/27/2023
+ * Description: program for CS2400 git lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
